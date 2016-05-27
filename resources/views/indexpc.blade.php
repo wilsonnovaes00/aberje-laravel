9bed789a69cdbc00110094ef4dc4e98f758722ac
Wilson Novaes <wilsonnovaes00@hotmail.com>
2016-05-27 15:09:08 -0300

    Add

--- resources/views/indexpc.blade.php
+++ resources/views/indexpc.blade.php
@@ -80,11 +80,11 @@
 
 
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min.js"></script>
     <script src="{{asset('js/custom.js') }} "> </script>
-    
+
 
 
 

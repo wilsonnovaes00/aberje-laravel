9c6e7a19ed9051b6dbaf6cba4efa653a04ca0407
Wilson Novaes <wilsonnovaes00@hotmail.com>
2016-05-27 22:17:16 -0300

    Add

--- public/js/custom.js
+++ public/js/custom.js
@@ -2,13 +2,9 @@ var app = angular.module('todoApp', [], function($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
 
-    function customersController($scope,$http) {
-        $http.get("/carregardados")
-            .success(function(datas) {$scope.datas = datas});
-
-    }
-
     
+
+
 
 });
 
